f9e386baacf9895539fa7b4ba0296fea7e7c0d29
Akash <yogibabu32@gmail.com>
2023-09-19 05:44:33 -0700

    removed background style

--- src/app/Component/Header/Header.js
+++ src/app/Component/Header/Header.js
@@ -1,9 +1,29 @@
-import React from "react";
+"use client"
+import React, { useEffect } from "react";
 import Link from "next/link";
 import styles from "./Header.module.css";
 import Image from "next/image";
 
 const Header = () => {
+useEffect(async()=>{
+  const url = 'https://moviesdatabase.p.rapidapi.com/titles/utils/lists';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'cba0739a46msh7a07a16164e30a0p18afa7jsn2eaeb6143444',
+		'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+},[])
+  
   return (
     <div className={styles.container}>
       <div>
